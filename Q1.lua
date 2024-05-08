d796175a71177b83cbc68ef99fa53c015ef06c04
Jorge Rincon <jorgerincon16@gmail.com>
2024-05-08 15:35:59 -0500

    Add notes and edits to Q1.

--- Q1.lua
+++ Q1.lua
@@ -1,13 +1,16 @@
 -- Q1 - Fix or improve the implementation of the below methods
 
 local function releaseStorage(player)
+    -- NOTE: Set storage value to 1000 with a -1 flag?
     player:setStorageValue(1000, -1)
 end
 
 function onLogout(player)
+    -- NOTE: when player object logs out, run this function. 
     if player:getStorageValue(1000) == 1 then
-    addEvent(releaseStorage, 1000, player)
+        -- EDIT: Fix function call
+        addEvent(releaseStorage(player))
     end
-    
+
     return true
 end
